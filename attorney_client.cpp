57d41d150bc9829eeaedf9812097c8ca28bfaa76
xuexcy <xuechengyunxue@gmail.com>
2024-10-21 21:19:45 +0800

    update attorney_client

--- attorney_client.cpp
+++ attorney_client.cpp
@@ -8,26 +8,33 @@
 // class Bar(friend Attorney);
 // 这样 class Bar 能访问哪些 class Client 的 private 成员可以用 class Attorney 来控制
 
+// c++ 中的友元不能继承
+// 如果 Attorney 需要通过父类的友元关系来访问其子类的私有成员，只能是 virtual Base::function,并且该
+// virtual function 在子类中必须是 accessible 的,如 public 继承(class Derived : public Base)
+
 #include <print>
 #include <source_location>
 
 #define CURRENT(method_name) \
     std::source_location::current().method_name()
+#define CURRENT_FUNCTION_NAME CURRENT(function_name)
 // #define GET_NAME_API(class_name) \
 //     inline static std::string_view get_name() { return #class_name; }
+
+namespace attorney {
 class ClientV1 {
 private:
     // GET_NAME_API(ClientV1)
     void A(int a) {
-        std::println("call {} ", CURRENT(function_name));
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         std::println("a: {}", a);
     }
     void B(float b) {
-        std::println("call {} ", CURRENT(function_name));
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         std::println("b: {}", b);
     }
     void C(double c) {
-        std::println("call {} ", CURRENT(function_name));
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         std::println("c: {}", c);
     }
     friend class Bar;
@@ -36,22 +43,23 @@ private:
 class ClientV2 {
 private:
     void A(int a) {
-        std::println("call {} ", CURRENT(function_name));
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         std::println("a: {}", a);
     }
     void B(float b) {
-        std::println("call {} ", CURRENT(function_name));
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         std::println("b: {}", b);
     }
     void C(double c) {
-        std::println("call {} ", CURRENT(function_name));
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         std::println("c: {}", c);
     }
-    friend class Attorney;
+    friend class AttorneyAB;
+    friend class AttorneyC;
 };
 
-// 代理
-class Attorney {
+// 代理类
+class AttorneyAB{
 private:
     static void callA(ClientV2& c, int a) {
         c.A(a);
@@ -62,9 +70,18 @@ private:
     friend class Bar;
 };
 
+class AttorneyC {
+private:
+    static void callC(ClientV2& client, double c) {
+        client.C(c);
+    }
+    friend class AccessC;
+};
+
 class Bar {
 public:
     void access_client_v1() {
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         ClientV1 client;
         client.A(1);
         client.B(2.0f);
@@ -72,14 +89,79 @@ public:
     }
 
     void access_client_v2_by_attorney() {
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
         ClientV2 client;
-        Attorney::callA(client, 1);
-        Attorney::callB(client, 2.0f);
+        AttorneyAB::callA(client, 1);
+        AttorneyAB::callB(client, 2.0f);
     }
 };
 
+class AccessC {
+public:
+    void access_client_v2_by_attorney() {
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
+        ClientV2 client;
+        AttorneyC::callC(client, 2.0);
+
+    }
+};
+
+}  // namespace attorney
+
+
+
+
+
+
+
+
+
+
+
+int main();
+
+namespace attorney_inherit {
+
+class Base {
+private:
+    virtual void Func(int x) {
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
+        std::println("x: {}", x);
+    };
+    friend class Attorney;
+public:
+    virtual ~Base() {}
+};
+
+class Derived : public Base {
+private:
+    virtual void Func(int x) {
+        std::println("call {} ", CURRENT_FUNCTION_NAME);
+        std::println("x: {}", x);
+    }
+};
+
+
+class Attorney {
+private:
+    static void callFunc(Base& b, int x) {
+        b.Func(x);
+    }
+    friend int ::main();
+};
+
+}  // namespace attorney_inherit
+
 int main() {
-    Bar bar;
+    attorney::Bar bar;
     bar.access_client_v1();
     bar.access_client_v2_by_attorney();
+
+    attorney::AccessC c;
+    c.access_client_v2_by_attorney();
+
+    attorney_inherit::Base b;
+    attorney_inherit::Derived d;
+    attorney_inherit::Attorney::callFunc(b, 5);
+    attorney_inherit::Attorney::callFunc(d, 10);
 }
