7f7d184b7ffcc1e9a5b0e383c9eb49344f6fa7a3
xuexcy <xuechengyunxue@gmail.com>
2024-11-04 18:33:35 +0800

    add computational_constructor

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
     Number n3 = n1 + n2;
     n3 = n2;
     ```
-- [attach_by_initialization](src/attach_by_initialization.cc): ⭐⭐⭐⭐⭐ 在用户代码 main 函数开始前，通过全局变量 global variable 执行一些工具类的初始化流程
+- [attach_by_initialization](src/attach_by_initialization.cc): ⭐⭐⭐⭐⭐ 在用户代码 main 函数开始前，通过全局变量 global variable 执行一些工具类的初始化流程，比如 [g_main_decorator](src/utils/main_decorator.h)，在 main 开始前和结束后打印一些信息
 - [attorney_client](src/attorney_client.cc): ⭐⭐ 如果 `class A(friend class B)`，则 class B 可以访问 class A 的所有私有成员。如果我们希望 class B 只能访问 class A 的一部分私有成员，可以通过代理类 `class Attorney` 控制友元类对私有成员的访问范围，`class A(friend class Attorney); class Attorney(friend class B)`，这样只要在 class Attorney 中设置允许 class B 访问 class A 的私有成员的接口即可
 - [barton_nackman_trick](src/barton_nackman_trick.cc): ⭐⭐⭐ 在模板类中将 operator 设置成 friend，解决 operator 重载带模板参数的问题
 - [base_from_member](src/base_from_member.cc): ⭐⭐ 基类先于继承类成员构造，若基类构造依赖继承类的成员，构造结果将可能不符合预期。通过将基类依赖的成员提取到单独的 class Container， 再由继承类控制继承顺序， 来解决此类问题
@@ -19,6 +19,7 @@
 - [checked_delete](src/checked_delete.cc): ⭐⭐⭐ 在不知道 class 完整定义时调用 `delete obj_ptr`， 代码编译会出现 warning 并编译成功，但运行结果是未定义，通过在 delete 前使用 `char[sizeof(T)?1:-1]` 让在此种情况下编译失败，以防止运行时结果不符合预期
 - [clear_and_minimize](src/clear_and_minimize.cc): ⭐⭐⭐⭐⭐ 容器在占用大量内存(capacity很大)且 clear 后，程序不会回收这些内存，造成内存浪费，通过空的容器与其 swap 可以回收，比如 `std::vector<int>().swap(v)`
 - [coercion_by_member](src/coercion_by_member.cc): ⭐⭐ Derived* 可以转换成 Base*，但是封装后的不行，比如 `std::unique_ptr<Base> b = std::unique_ptr<Derived>()`，因为两个 unique_ptr 没有继承关系。通过模板封装 Derived 并实现拷贝构造和赋值构造，可以主动将 Derived* 转换为 Base*，如模板类 `Ptr<class T>` 实现相关构造函数后，可以将 Derived 指针转换为基类指针 `Ptr<Base> b = Ptr<Derived>()`
+- [computational_constructor](src/computational_constructor.cc): ⭐ 对于不支持 NRVO(named return value optimization) 的编译器，将函数中的计算并 return obj 的逻辑，改成 return Object(xxx) 的方式(即由构造函数实现计算逻辑并直接 return 构造结果)，也就是通过构造 + RVO 的方式来替代 NRVO
 - [curiously_recurring_template_pattern](src/curiously_recurring_template_pattern.cc): ⭐⭐⭐⭐ 奇异递归模板模式， 基类模板参数为继承类，实现编译期多态，即在编译期确定在基类函数中调用哪个子类函数
 - [interface_class](src/interface_class.cc): ⭐⭐⭐⭐ 在类中声明纯虚函数接口，实现接口类 class Interface
 
---
+++ src/computational_constructor.cc
@@ -0,0 +1,173 @@
+/**
+########################################################################
+#
+# Copyright (c) 2024 xx.com, Inc. All Rights Reserved
+#
+########################################################################
+# Author  :   xuechengyun
+# E-mail  :   xuechengyun@gmail.com
+# Date    :   2024/11/04 15:51:47
+# Desc    :
+########################################################################
+
+在一些不支持 NRVO 的编译器上，通过将实例的计算逻辑放到构造函数，然后通过使用 RVO 进行替代
+也就是不用 {
+    Object obj;
+    xxx;
+    xxx;
+    return obj;
+}, 而是使用 {
+    return Object(xxx);
+}
+
+rvo: return value optimization 返回值优化，函数返回一个新构造的临时对象(未命名)，比如 return Data(c)
+Nrvo: named return value optimization 命名返回值优化，函数返回一个已命名对象，比如
+    Data c;
+    c.set_data('c');
+    return c;
+*/
+
+#include <algorithm>
+#include <new>
+#include <print>
+
+#include "cpp_utils/util.h"
+
+#include "utils/main_decorator.h"
+
+struct Data {
+    Data(char c = 0) {
+        std::println("{}", CURRENT_FUNCTION_NAME);
+        std::fill(bytes, bytes + 16, c);
+    }
+    Data(const Data& d) {
+        std::copy(d.bytes, d.bytes + 16, this->bytes);
+        std::println("A copy was made.\n");
+    }
+    void print() {
+        std::println("{}", bytes);
+    }
+    ~Data() {
+        std::println("{}", CURRENT_FUNCTION_NAME);
+    }
+private:
+    char bytes[16];
+};
+
+namespace bad_case_rvo {
+Data run_copy_and_assign(char c) {
+    return Data(c);
+}
+
+void run() {
+    std::println("Start bad_case_rvo");
+    // 这里编译器会优化，最终在 run_copy_and_assign 函数里调用一次构造函数，然后在 run 函数结束后调用一次 d 的析构函数
+    // 如果没有 rvo，那么在调用 run_copy_and_assign 时，会传入一个临时变量的地址，然后
+    //  1. 在 run_copy_and_assign 中构造 Data(c)
+    //  2. 在 run_copy_and_assign 中 return 时，使用 Data(c) 拷贝构造到这个临时变量
+    //  3. 在此处 auto d = xxx; 时将临时变量赋值(拷贝)构造到 d
+    // 于是发生了 1次普通构造+2次拷贝构造，最后再发生三次析构
+    auto d = run_copy_and_assign('c');
+    d.print();
+    std::println("End bad_case_rvo");
+}
+}  // namespace bad_case_rvo
+
+namespace solution_rvo {
+// 在没有 rvo 的情况下，可以使用 placement new 来实现 rvo，但是最后要自己主动调用析构函数
+void run_rvo(Data* target, char c) {
+    new (target) Data(c);
+    return;
+}
+void run() {
+    std::println("Start solution_rvo");
+    char bytes[sizeof(Data)];
+    auto target = reinterpret_cast<Data*>(bytes);
+    run_rvo(target, 'r');
+    target->print();
+    target->~Data();
+    std::println("End solution_rvo");
+}
+}  // namespace solution_rvo
+
+namespace bad_case_nrvo {
+class File {
+private:
+    std::string str_;
+public:
+    File() {}
+    void path(std::string_view path) {
+        str_ += path;
+    }
+    void name(std::string_view name) {
+        str_ += '/';
+        str_ += name;
+    }
+    void ext(std::string_view ext) {
+        str_ += '.';
+        str_ += ext;
+    }
+    void print() {
+        std::println("{}", str_);
+    }
+};
+
+File getfile() {
+    File f;
+    f.path("/lib");
+    f.name("libc");
+    f.ext("so");
+    f.ext("6");
+    // no RVO because object has a name 'f'
+    // not all compiler support NRVO
+    return f;
+}
+
+void run() {
+    std::println("Start bad_case_nrvo");
+    auto f = getfile();
+    f.print();
+    std::println("End bad_case_nrvo");
+}
+
+} // namespace bad_case_nrvo
+
+namespace solution_nrvo {
+// 通过将计算逻辑放到构造函数，避免使用一个 named variable
+// 这样可以通过编译器的 rvo 来做到 nrvo
+class File {
+private:
+    std::string str_;
+public:
+    File() {}
+    File(
+        const std::string& path, const std::string& name,
+        const std::string& ext1, const std::string& ext2)
+    :str_(path + "/" + name + "." + ext1 + "." + ext2) {
+    }
+    void print() {
+        std::println("{}", str_);
+    }
+};
+
+File getfile() {
+    return File("/lib",  "libc", "so", "6");
+}
+
+void run() {
+    std::println("Start solution_nrvo");
+    auto f = getfile();
+    f.print();
+    std::println("End solution_nrvo");
+}
+}  // namespace solution_nrvo
+
+int main() {
+    bad_case_rvo::run();
+    solution_rvo::run();
+
+    bad_case_nrvo::run();
+    solution_nrvo::run();
+    return 0;
+}
+
